d2e3d9ca96a5150d388a9d47fbd9907cb8d71c9a
Denis Zavadskiy <denis.v.zavadskiy@gmail.com>
2016-09-29 01:22:55 +0300

    add(styles, images): to index page

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,26 @@
-(function () {
-    'use strict';
+// (function () {
+//     'use strict';
+//
+//     var fragment = document.createDocumentFragment(),
+//         link = document.querySelectorAll('link[rel=import]'),
+//         header = link[0].import.querySelector('header'),
+//         footer = link[1].import.querySelector('footer');
+//
+//     fragment.appendChild(header);
+//     fragment.appendChild(footer);
+//
+//     document.body.appendChild(fragment);
+// })();
 
-    var fragment = document.createDocumentFragment(),
-        link = document.querySelectorAll('link[rel=import]'),
-        header = link[0].import.querySelector('header'),
-        footer = link[1].import.querySelector('footer');
-
-    fragment.appendChild(header);
-    fragment.appendChild(footer);
-
-    document.body.appendChild(fragment);
-})();
-
+jQuery(function($){
+    // bind event handlers to modal triggers
+    $('body').on('click', '.trigger', function(e){
+        e.preventDefault();
+        $('#test-modal').modal().open();
+    });
+    // attach modal close handler
+    $('.modal .close').on('click', function(e){
+        e.preventDefault();
+        $.modal().close();
+    });
+});
