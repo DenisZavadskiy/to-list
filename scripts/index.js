6ce03ce8bec050d00caa3e2c00bb571020d16630
Denis Zavadskiy <denis.v.zavadskiy@gmail.com>
2016-10-02 19:17:16 +0300

    update(work-description markup and style)

--- scripts/index.js
+++ scripts/index.js
@@ -1,17 +1,3 @@
-// (function () {
-//     'use strict';
-//
-//     var fragment = document.createDocumentFragment(),
-//         link = document.querySelectorAll('link[rel=import]'),
-//         header = link[0].import.querySelector('header'),
-//         footer = link[1].import.querySelector('footer');
-//
-//     fragment.appendChild(header);
-//     fragment.appendChild(footer);
-//
-//     document.body.appendChild(fragment);
-// })();
-
 jQuery(function ($) {
     // bind event handlers to modal triggers
     $('body').on('click', '.trigger', function (e) {
