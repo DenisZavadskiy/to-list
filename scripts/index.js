e88554fdfadf06ffb5e9e347decbd8580ecd0089
Denys Zavadskyi <denis.v.zavadskiy@gmail.com>
2017-01-16 23:21:37 +0200

    started dialog view

--- scripts/index.js
+++ scripts/index.js
@@ -2,13 +2,15 @@ jQuery(function ($) {
     var cachedWidth = $(window).width();
 
     $('body').on('click', '.appointment-trigger', function (e) {
-        $('#overlay').css("display", "block");
+        $('#overlay').css("display", "flex");
         $('#calendar-modal-wrapper').css("display", "block");
         $("body").addClass("themodal-lock");
     }).on('click', '#overlay', function (e) {
-        $('#overlay').css("display", "none");
-        $('#calendar-modal-wrapper').css("display", "none");
-        $("body").removeClass("themodal-lock");
+        if (e.target === e.currentTarget) {
+            $('#overlay').css("display", "none");
+            $('#calendar-modal-wrapper').css("display", "none");
+            $("body").removeClass("themodal-lock");
+        }
     }).on('click', '.trigger', function (e) {
         e.preventDefault();
         $('#test-modal').modal().open();
