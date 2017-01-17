5cb38a2831a4ca085774cae841a781934bf17ca3
Denys Zavadskyi <denis.v.zavadskiy@gmail.com>
2017-01-17 18:42:59 +0200

    add login form, finish dialog view

--- scripts/index.js
+++ scripts/index.js
@@ -4,12 +4,12 @@ jQuery(function ($) {
     $('body').on('click', '.appointment-trigger', function (e) {
         $('#overlay').css("display", "flex");
         $('#calendar-modal-wrapper').css("display", "block");
-        $("body").addClass("themodal-lock");
+        // $("body").addClass("themodal-lock");
     }).on('click', '#overlay', function (e) {
         if (e.target === e.currentTarget) {
             $('#overlay').css("display", "none");
             $('#calendar-modal-wrapper').css("display", "none");
-            $("body").removeClass("themodal-lock");
+            // $("body").removeClass("themodal-lock");
         }
     }).on('click', '.trigger', function (e) {
         e.preventDefault();
@@ -21,6 +21,8 @@ jQuery(function ($) {
                 $(this).find('.submenu').css("display", "none");
             });
         }
+    }).on('click', '.close', function (e) {
+        $('#overlay').css("display", "none");
     });
 
     $(document).on('click', function (e) {
@@ -43,5 +45,30 @@ jQuery(function ($) {
     $('.modal .close').on('click', function (e) {
         e.preventDefault();
         $.modal().close();
+    });
+
+    var inputs = document.querySelectorAll( '.inputfile' ),
+        attachFileLabel = document.getElementsByClassName("attach-file-label")[0];
+
+    Array.prototype.forEach.call( inputs, function( input )
+    {
+        var label = attachFileLabel;
+            labelVal = label.innerHTML;
+
+        input.addEventListener( 'change', function( e )
+        {
+            var fileName = '';
+
+            if( this.files && this.files.length > 1 ) {
+                fileName = ( this.getAttribute('data-multiple-caption') || '' ).replace('{count}', this.files.length);
+            } else {
+                fileName = e.target.value.split('\\').pop();
+            }
+            if( fileName ) {
+                label.querySelector('span').innerHTML = fileName;
+            } else {
+                label.innerHTML = labelVal;
+            }
+        });
     });
 });
